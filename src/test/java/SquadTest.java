264690974bda514af01fcac35653a006f6cb3b43
David <kingdice@Atomic>
2022-05-01 15:15:59 +0300

    here we are writing the initial tests for our squad class..intialize our exception handling. we are also checking whether our squad is correctly instantiated

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,2 +1,39 @@
+import models.Hero;
+import models.Squad;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SquadTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    private Hero setupNewHero() {
+        return new Hero("Batman", 48, "Money", "Loneliness");
+    }
+
+    private Hero setupHero2() {
+        return new Hero("Iron Man", 30, "Money", "Ego");
+    }
+
+    private Squad setupNewSquad(Hero hero) {
+        return new Squad("Justice League", "Just Exist", hero);
+    }
+    private Squad setupNewSquad2(Hero hero) {
+        return new Squad("Avengers", "Just Exist again", hero);
+    }
+
+    @Test
+    public void squadInstanciatedCorrectly() {
+        Squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad instanceof Squad);
+    }
+
+    @Test
+    public void getSquadName() {
+        Squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad.getName() instanceof String);
+    }
 }
