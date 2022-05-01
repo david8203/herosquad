fba4a7fedcf369f24016dab1cebb16f96423be50
David <kingdice@Atomic>
2022-05-01 15:01:28 +0300

    here we are using an if statement to delete a hero and check whether he or she is in a squad  and also update our user string input and we create a constructor in our hero squad.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,29 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String squadName;
+    private String cause;
+    private static boolean isRegisteredHero = false;
+    private List<Hero> heroMembers = new ArrayList<>();
+    private static List<Squad> squadList = new ArrayList<>();
+    private int squadId;
+    private boolean isSquadFull = false;
+
+    public Squad(String name, String cause, models.Hero hero) {
+        this.squadName = name.trim();
+        this.cause = cause.trim();
+        crossCheckHero(hero.getHeroID());
+
+        if (isRegisteredHero) {
+            hero.setSquadAlliance(squadName);
+            heroMembers.add(hero);
+            squadList.add(this);
+            this.squadId = squadList.size();
+        } else {
+            System.out.println("HERO ISN'T REGISTERED");
+        }
+    }
 }
