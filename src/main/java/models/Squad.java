e95c2f9e57764fcd66631c124d524523eb5dd9a4
David <kingdice@Atomic>
2022-05-02 11:55:35 +0300

    in the hero squad class we are using an if conditional statement to check whether we can successfully change a hero from one squad to another and whether we can create a new squad.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -41,4 +41,49 @@ public class Squad {
             heroMembers.add(hero);
         }
     }
+    public boolean getSquadFull() {
+        return isSquadFull;
+    }
+
+    public List<models.Hero> getMembers() {
+        return heroMembers;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+    public void changeHeroSquad(models.Hero hero, Squad newSquad) {
+        if (heroMembers.size() >= 3) {
+            isSquadFull = true;
+        } else {
+            Squad currentSquad = null;
+            for (Squad squad : squadList) {
+                if (hero.getSquadAlliance().equalsIgnoreCase(squad.squadName)) {
+                    currentSquad = squad;
+                    break;
+                }
+            }
+
+            for (Squad squad : squadList) {
+                if (newSquad.squadName.equalsIgnoreCase(squad.squadName)) {
+
+                    if (!hero.getSquadAlliance().equalsIgnoreCase("")) {
+                        //IF HERO EXISTED IN PREVIOUS SQUAD
+                        currentSquad.heroMembers.remove(hero);
+                        newSquad.heroMembers.add(hero);
+                        hero.updateSquad(newSquad.squadName);
+                        break;
+
+                    } else {
+                        //IF HERO HAD NO ALLIANCE
+                        newSquad.heroMembers.add(hero);
+                        hero.setSquadAlliance(newSquad.squadName);
+                    }
+
+                } else {
+                    System.out.println("Squad Doesn't exist");
+                }
+            }
+        }
+    }
 }
